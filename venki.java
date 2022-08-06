b461d27a6b546c7252a2c22d842d43f0071778ef
veeravallivenki <veeravallikrishna293@gmail.com>
2022-08-06 11:47:27 +0530

    initial commit

---
+++ venki.java
@@ -0,0 +1,8 @@
+ hi good morning
+ i'm venkata krishna
+ i'm having 3 years of experience on devops tools 
+ past 3 years i have been working in Accenture as devops engineer
+  
+ Well coming to my qualifcation 
+  i have completed my post-graduation from andhra university at tanuku aggregate with 7.8 cgpa
+
